703ca06e083219b61a93628b9f66f8c5f2d384de
Ankit Kumar <ankitkumargcet@gmail.com>
2024-11-06 23:10:13 +0530

    included package

---
+++ src/main/java/com/e2e/common/config/TestImplementation.java
@@ -0,0 +1,12 @@
+package com.e2e.common.config;
+
+/**
+ * @author ankit
+ */
+public class TestImplementation {
+
+    public static void display(String input) {
+        System.out.println("Your input is printing from com.e2e.common.config.E2ETestImplementation : " + input);
+    }
+
+}
